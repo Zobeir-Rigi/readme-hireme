a899f476408e0361a2c94ff29a1b35aaded745ed
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-23 05:36:11 +0000

    database changed. body parser installed and post endpoint to creat graduate created

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Pool } = require("pg");
+const bodyParser = require("body-parser"); 
 
 require("dotenv").config();
 
@@ -7,7 +8,7 @@ const app = express();
 /********************** database config */
 const pool = new Pool({
     host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_NAME || "Graduate",
+    database: process.env.DB_NAME || "Graduate-tracker",
     user: process.env.DB_USER || "", 
     password: process.env.DB_PASS || process.env.DB_PASSWORD || "",
 	port : process.env.DB_PORT || "5432" ,
@@ -24,6 +25,9 @@ const pool = new Pool({
   };
   const port = process.env.PORT || 8800;
 /**************************************** */
+// app.use(express.json())
+app.use(bodyParser.json()); // Middleware to parse JSON in the request body
+
 app.get("/", (req, res)=>{
     res.json("Hello, this is the Backend")
 })
@@ -32,9 +36,31 @@ app.get("/graduates", (req, res)=>{
     const q = "select * from graduates"
     db.query(q, (err, data)=>{
         if(err) return res.json(err)
-        return res.json(data)
+        return res.json(data.rows)
     })
 })
+
+
+app.post("/addgraduate", (req, res) => {
+  const { full_name, github_link, linkedIn_link, portfolio_link, role, about_me, skills } = req.body;
+
+  const q = `
+      INSERT INTO graduates 
+      (full_name, github_link, linkedIn_link, portfolio_link, role, about_me, skills) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+  `;
+
+  const values = [full_name, github_link, linkedIn_link, portfolio_link, role, about_me, skills];
+
+  db.query(q, values, (err, data) => {
+      if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Error adding graduate to the database." });
+      }
+      return res.status(200).json({ message: "Graduate has been added successfully." });
+  });
+});
+
 app.listen(port, ()=>{
     console.log("connected to the Backend")
 })
