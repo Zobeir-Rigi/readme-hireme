df07ff25d665503450058ba50f96e03be03c8949
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-23 06:26:32 +0000

    cors installed and i could fetch all data

--- client/src/pages/Graduates.jsx
+++ client/src/pages/Graduates.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 export const Graduates = ()=>{
+    const [graduates, setGraduates] = useState([])
+
+    useEffect(()=>{
+        const fetchAllGraduates = async ()=>{
+            try{
+                const res = await axios.get("http://localhost:8800/graduates")
+                console.log(res)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchAllGraduates()
+    },[])
     return(
         <div>
             All Graduates cards
