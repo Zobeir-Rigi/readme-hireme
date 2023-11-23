a812d10272324d3e88bcec143dee9b04e9eb6141
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-23 14:04:49 +0000

    home.js & css  created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
-export const Home = ()=>{
-    return(
-        <div>
-            LandingPage
+import landingPic from "../Pics/landingPicture.jpg"
+import githubIcon from "../Pics/github-mark.svg"
+import "./home.css"
+import { Link } from "react-router-dom"
+export const Home = () => {
+    return (
+        <div className="landing-Container">
+            <div className="firstSection">
+                <img className="landingPic" src = {landingPic} alt="landingPic" />
+                <p className="landingText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores 
+                    ipsam nisi maxime, repellat amet repellendus soluta laborum dignissimos 
+                    porro! Quis tempore libero repudiandae culpa ad architecto quam praesentium 
+                    eius repellendus?
+                    Lorem ipsum dolor sit amet</p>
+            </div>
+            <div className="bottom-section">
+                <div className="section-two">
+                    <Link className="link" to="/graduates"><span>See Our Wonderfull Graduates</span></Link>
+                </div>
+                <div className="section-three">
+                    <img className="githubSvg" src= {githubIcon} alt="" />
+                    <span>Log In With Github</span>
+                </div>
+            </div>
         </div>
-    )
-}
+
+    );
+    
+} 
