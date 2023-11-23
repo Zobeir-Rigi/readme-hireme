48885f3526fd34c6090f791e07a144dc62e74e02
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-23 08:07:32 +0000

    graduates table mapped and graduate card added

--- client/src/pages/Graduates.jsx
+++ client/src/pages/Graduates.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+import  "./graduates.css"
+import portfolioBag from"../Pics/portfolio-bag.svg"
+
+
+import { Link } from "react-router-dom"
 
 export const Graduates = ()=>{
     const [graduates, setGraduates] = useState([])
@@ -8,7 +13,8 @@ export const Graduates = ()=>{
         const fetchAllGraduates = async ()=>{
             try{
                 const res = await axios.get("http://localhost:8800/graduates")
-                console.log(res)
+                // console.log(res)
+                setGraduates(res.data)
             }catch(err){
                 console.log(err)
             }
@@ -17,7 +23,24 @@ export const Graduates = ()=>{
     },[])
     return(
         <div>
-            All Graduates cards
+            <h1>Cyf Graduates</h1>
+            <div className="allGraduates">
+                {
+                    graduates.map(graduate =>(
+                        <div className="graduate-card" key={graduate.full_name}>
+                            {/* <img  src= {githubData.avatar} alt="" /> */}
+                            <h2>{graduate.full_name}</h2>
+                            <h3>{graduate.role}</h3>
+                            <div className="icons-section">
+                                <i  className="icon fa-brands fa-linkedin-in"></i> 
+                                <i className="icon fa-brands fa-github"></i>
+                                <img  className="portfolioSvg icon" src= {portfolioBag} alt="" />
+                            </div>
+                           <Link className="link" to={"/graduate"}> <h4>more</h4></Link>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
