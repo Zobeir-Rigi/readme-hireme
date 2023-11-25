c98dd84fe7a7646a43a65ffa2b0efabbff77356d
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-25 07:57:10 +0000

    footer added , css updated

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,16 @@ import {CreateGraduate} from "./pages/CreateGraduate";
 import {Graduates} from "./pages/Graduates";
 import {GraduateDetail} from "./pages/GraduateDetail"
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
 import { UserProvider } from './UserContext';
 
 export const App = () => {
   return (
     <div className="App">
     <BrowserRouter>
-    <UserProvider>
     <Header />
+    <UserProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/graduates" element={<Graduates />} />
@@ -20,6 +22,7 @@ export const App = () => {
         <Route path='/createGraduate' element={<CreateGraduate />} />
       </Routes>
     </UserProvider>
+    <Footer />
     </BrowserRouter>
     </div>
   );
---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css'; 
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Your Website Name. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
