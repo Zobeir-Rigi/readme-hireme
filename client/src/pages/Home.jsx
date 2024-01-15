418ff279e0d9b68e132282d5d59d44f94f9ebee3
zobeir-rigi <rigi.zobir@yahoo.com>
2024-01-15 23:50:35 +0000

    need to delete the comments

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import landingPic from "../Pics/landingPicture.jpg"
-// import githubIcon from "../Pics/github-mark.svg"
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useState } from "react";
